1f16429b990d95b5b955633e28d0ce3ec85e0c60
João Pedro Leite S Lisboa <joaopedroleite.s.lisboa.info@gmail.com>
2020-09-07 19:01:21 -0300

    Ajustes necessarios para funcionamento do Projeto

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api/v1/people")
+@RequestMapping("/v1/people")
 public class PersonController {
 
     private PersonService personService;
